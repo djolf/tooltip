55d3d6cc8d001798d18d8f8bd16469c3c1a6a3b5
lin lifan <lifan1312@gmail.com>
2018-06-08 22:55:58 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// var ref = $('.btn');
+tippy('[title]', {
+    arrow: false,
+    placement: 'top',
+    theme: 'mom',
+    animateFill: false,
+    animation: '',
+    offset: '0, 10',
+    duration: [0, 0]
+    // trigger: 'click'
+});
+
+// var popper = new Tooltip(ref, {     placement: 'top-start',     title:
+// "AHAHAH",     offset: 'top 10',     popperOptions: {         onCreate:
+// function (data) {             console.log(data);         }     } });
+
+function makeTooltip(ref) {}
